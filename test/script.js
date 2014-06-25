098c69b97f3824c20a08f027ded33ab52e85aa86
Matt Nibecker <mnibecke@gmail.com>
2014-06-24 21:53:17 -0700

    some general house cleaning

---
+++ test/script.js
@@ -0,0 +1,23 @@
+
+angular.module("TestApp", ['angular-edit-row'])
+
+function TestCtrl($scope) {
+  $scope.tracks = [
+    { No: 1, Title: "Let's Go For A Ride", Artist: "Rac" },
+    { No: 2, Title: "How Great is God", Artist: "Hillsong" }
+  ];
+}
+
+function EditTrackCtrl($scope, $rowEditInstance) {
+	$scope.editTrack = angular.copy($scope.track);
+
+	$scope.save = function() {
+		$scope.track.Title = $scope.editTrack.Title;
+		$rowEditInstance.close();
+	}
+
+	$scope.delete = function() {
+		$rowEditInstance.close();
+	}
+
+}
